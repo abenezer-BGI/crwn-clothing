447e3ba025c7bf9456cec013392f4fb6d1e08539
abenezer-BGI <abenezer.ayalneh@castel-afrique.com>
2021-11-07 10:58:21 +0300

    updated the menu-item with navigaiton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
+import {Routes,Route} from "react-router-dom";
+
+const HatsPage = ()=>{
+    return (
+        <div>Hats Page</div>
+    )
+}
 
 function App() {
     return (
         <div>
-            <HomePage/>
+            <Routes>
+                <Route exact path='/' element={<HomePage/>}/>
+                <Route path='/shop/hats' element={<HatsPage/>}/>
+            </Routes>
         </div>
     );
 }
