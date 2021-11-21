3ffd959aa31986f60a2ab24134ad47efab0cbe6e
abenezer-BGI <abenezer.ayalneh@castel-afrique.com>
2021-11-21 10:45:04 +0300

    new shop data with unique id, add items to cart redux implementation and grouping items

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 import {ReactComponent as Logo} from "../../assets/crown.svg";
 import {auth} from "../../firebase/firebase.util";
 import {connect} from "react-redux";
-import CartIcon from "../car-icon/cart-icon.component";
+import CartIcon from "../cart-icon/cart-icon.component";
 import CardDropdown from "../cart-dropdown/cart-dropdown.component";
 
 const Header = ({currentUser,cartDropdownHidden}) => (
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -3,3 +3,8 @@ import {CartActionType} from "./cart.action-types";
 export const toggleCartDropdownHidden = () => ({
     type: CartActionType.TOGGLE_CART_DROPDOWN_HIDDEN,
 })
+
+export const addItem = (item) => ({
+    type: CartActionType.ADD_ITEM,
+    payload: item
+})
